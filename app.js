436c0758ee34579200080f3c65126ff0df5d1508
Ritwik <luciferbusiness3@gmail.com>
2020-04-07 23:38:42 +0530

    Created .gitignore, inserted herroku port to app.js file & added node version in package.json file

--- app.js
+++ app.js
@@ -134,6 +134,11 @@ app.post("/delete", (req, res) => {
 
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000.");
+let port = process.env.PORT;
+if (port == nul || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log(`Server started on port ${port}.`);
 });
